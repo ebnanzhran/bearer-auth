94c265e9e2ce97fe0c2300a18143323ce7cfc21c
ebnanzhran <bnanzhran515@gmail.com>
2022-06-15 14:44:46 +0300

    done

---
+++ src/middlewares/bearer.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const {Users} = require("../module/Users");
+
+async function bearer(req, res, next) {
+    if (req.headers.authorization) {
+        
+        console.log(req.headers.authorization);
+        const bearerToken = req.headers.authorization.split(" ")[1];
+        Users.authenticateBearer(bearerToken)
+            .then((userData) => {
+                req.user = userData;
+                next();
+            })
+            .catch(() => {
+                res.status(403);
+                res.send("Invalid Signin");
+            })
+    }
+}
+
+module.exports = bearer;
